05f367c95cc118a649bcc1aee1d34e75a49ca577
sergChornohor <chornohor1992@gmail.com>
2019-11-14 12:27:03 +0200

    2#lesson

--- js/main.js
+++ js/main.js
@@ -5,16 +5,14 @@ let photo_array = document.querySelectorAll('.uploaded-photos .uploaded-photo');
 let photo_notification = document.getElementById('notifications'); 
 
 completed.onclick = function() {
-    confirm('Are you sure you want to change the number of tasks?');
-
+    
     if (openNumber.innerHTML <= 0) {
         alert('There is no open tasks');
-        openNumber.innerHTML = 0;
-        completedNumber.innerHTML = completedNumber.innerHTML;
     } else {
-       completedNumber.innerHTML = Number(completedNumber.innerHTML) + 1;
-        openNumber.innerHTML = Number(openNumber.innerHTML) - 1;
-     }
+        confirm('Are you sure you want to change the number of tasks?');
+        completedNumber.innerHTML++;
+        openNumber.innerHTML--;
+    }
  }
 
 let photo_array_listner = function(image, index) {
@@ -26,4 +24,3 @@ let photo_array_listner = function(image, index) {
 for  (i = 0; i < photo_array.length; i++) {
     photo_array_listner(photo_array[i], i);
 }
-
